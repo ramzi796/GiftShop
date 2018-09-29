c87cc0d782d9624837e98f883ac830eb303bc874
unknown <ramzi796@gmail.com>
2018-09-29 12:55:07 +0530

    Adding Sign Up Skeleton

--- src/app/landing/signup/signup.component.ts
+++ src/app/landing/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
+  selector: 'gs-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  form = new FormGroup({
+    email: new FormControl,
+    password: new FormControl
+  });
 
   constructor() { }
 
